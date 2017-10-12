33d1bf63de0fb9382a141c2d195b365d0757df85
mkalikas <mk134@knuth2.cs.uakron.edu>
2017-10-12 16:59:41 -0400

    added new main.cc file

--- Project1Part2/main.cc
+++ Project1Part2/main.cc
@@ -7,7 +7,7 @@
 #include <sys/stat.h>
 #include "sha256.h"
 #include "BigIntegerLibrary.hh"
-#include <cassert>
+
 
 // Parameters are a file name and a length, used for signature length
 // This function reads a file and generates a hash
@@ -24,8 +24,10 @@ std::string read_file(std::string filename = "file.txt") {
   infile.read (memblock, length); //read the entire file
   memblock[length] = '\0';
   infile.close();
+  std::string str(memblock);
+
   delete [] memblock;
-  return static_cast<std::string>(memblock); // turns memblock into a string; This is the entire original file as a string representation
+  return str; // turns memblock into a string; This is the entire original file as a string representation
 
 }
 
@@ -35,7 +37,7 @@ std::string read_file(std::string filename = "file.txt") {
   It also takes in the hash of a message.
   Returns a string of the encrypted/decrypted message.
 */
-std::string cryptomessage(std::string key_filename, BigUnsigned base) {
+std::string cryptomessage(std::string key_filename, BigInteger base) {
   std::ifstream input_key(key_filename.c_str());
   std::string key, n; // strings to hold the values extracted from the file
   for(int i = 0; i < 2; ++i) {
@@ -47,73 +49,70 @@ std::string cryptomessage(std::string key_filename, BigUnsigned base) {
   input_key.close();
 
   /****** Encrypt/decrypt using the hash using the key ******/
-  return bigUnsignedToString(modexp(BigInteger(base), stringToBigUnsigned(key), stringToBigUnsigned(n)));
+  return bigUnsignedToString(modexp(base, stringToBigUnsigned(key), stringToBigUnsigned(n)));
 }
 
 /*
   Takes a string of the name of a file to be read. The file is read by calling the read_file function.
   The input file is modified by adding a signature, decrypted string of the input file's contents, to the end of the original file.
-  Returns the name of the modified input file.
 */
 void generate_signature(std::string filename) {
-  //filename = filename+".signed";
-  std::string memblock = read_file(filename); // get the contents of the original file as a string
-  /*std::ifstream infile(filename.c_str(), std::ios::binary);
-  std::streampos begin,end;
-  begin = infile.tellg();
-  infile.seekg (0, infile.end);
-  end = infile.tellg();
-  std::streampos length = end-begin; //size of the file in bytes
-  infile.seekg (0, infile.beg);
-
-  char* memblock = new char[length];
-  infile.read (memblock, length); //read the entire file
-  memblock[length] = '\0';
-  infile.close();
-  */
   filename = filename+".signed";
+  std::string memblock = read_file(filename); // get the contents of the original file as a string
   std::ofstream outfile (filename.c_str(), std::ios::binary);
-  char* content = new char[length];
-  strcpy(content, memblock);
-  outfile.write (content, length); // writes the contents of the original file to the signed file
+  char* content = new char[memblock.length()];
+  strcpy(content, memblock.c_str());
+  outfile.write (content, memblock.length()); // writes the contents of the original file to the signed file
   int sigLength = 1024; // length of the signature
   char* signature = new char[sigLength]; // character array to hold the signature
   signature[sigLength] = '\0'; // add a terminator
+  // Turn memblock into a BigInteger
+  BigInteger base = dataToBigInteger(memblock.c_str(), memblock.length(), BigInteger::positive);
   // Make call to cryptomessage to get the decrypted string of memblock
-  std::string decrypt = cryptomessage("d_n.txt", BigUnsignedInABase(sha256(memblock), 16)); // pass the file holding the private key and n
+  std::string decrypt = cryptomessage("d_n.txt", base); // pass the file holding the private key and n
   strcpy(signature, decrypt.c_str()); // copy the decrypted string into the character array
   outfile.write (signature, sigLength); // write the signature to the file
   outfile.close();
-  delete [] memblock;
+  //delete [] memblock;
   delete [] content;
   delete [] signature;
 
-  //return filename; // used to get name of signed file that is sent
 }
 
-// Takes two strings as arguments. string for the name of the file to check the signature
+// Takes a string representing the name of the file to be checked
 // Returns true if the document is authentic, and false otherwise.
-bool verify_signature(std::string signed_file) {
-  //std::string original_file = read_file(original); // gets the entire file as a string
-  std::string sign = signed_file+".signed"; //read_file(signed_file); // reads the entire file and signature
-  std::ifstream infile(signed_file.c_str(), std::ios::binary);
-  std::streampos begin,end;
-  begin = infile.tellg();
-  infile.seekg (0, infile.end);
-  end = infile.tellg();
-  std::streampos length = end-begin; //size of the file in bytes
-  infile.seekg (0, infile.beg);
+bool verify_signature(std::string check) {
+  // sets check to the entire contents of the file represented as a string
+  // check contains the entire file contents and signature, i.e this is expected to be the original content plus the signature
+  check = read_file(check);
 
-  char* memblock = new char[length];
-  infile.read (memblock, length); //read the entire file
-  memblock[length] = '\0';
-  infile.close();
-  delete [] memblock;
+  // create a new string named sign and initialize it to check
+  // this string will be used to separate the content from the signature
+  //std::string sign(check);
+  ////unsigned char buffer[check.length()];
+  //std::copy(check.c_str(), 1024, )
+  //const char* check_array[check.length()];
+//  strcpy(check_array, check.c_str());
+  const char* check_array = check.c_str(); // create a character array containing the cstring representation of check
+  char check2[(strlen(check_array) - 1024)]; // a charater array the size of the content
+  std::cout << check2 << "\n";
+  char* sign_array[1024]; // a character array the size of the signature
+  strncpy(check2, check.c_str(), sizeof(check2));
 
-  sign.erase(sign.begin(), sign.end()-1024); // erases the original from the signed_file to get the signed portion
-  std::string signature_encrypt = cryptomessage("e_n.txt", stringToBigUnsigned(sign)); // String that holds the signature encrypted
+
+
+  //check_array[check.length()-1024] = '\0';
+  //strcpy(check_array, check.c_str()); // copy only the lengh of the content into check_array
+  //sign.erase(sign.begin()+0, sign.end()-1024); // erases the original from the check to get the signed portion
+  //char* sign_array = new char[1024];
+
+  //std::cout << sign << "\n";
+  //check.erase(check+sign.length()); // erase the
+
+  //std::string signature_encrypt = cryptomessage("e_n.txt", stringToBigInteger(sign)); // String that holds the signature encrypted
   // Compare strings and return if they are equal
-  return (signature_encrypt.compare(sign) == true); // compares the signed_file to the encrypted version of it and returns if they are the same
+  //return (signature_encrypt.compare(sign) == true); // compares the check to the encrypted version of it and returns if they are the same
+  return true;
 }
 
 int main(int argc, char *argv[])
@@ -130,20 +129,6 @@ int main(int argc, char *argv[])
     default:
       return 0;
   }
-/*
-  if(argc > 1) {
-    // for loop used to pass each string to the function
-    for(int i = 1; i < argc; ++i) {
-      // pass argument to main and 0 because this is the original file
-      // and does not have a signature
-      std::string signed_file = generate_signature(argv[i]); // signed_file is the name of the file returned by passing an input file to the function
-      if((verify_signature(argv[i], signed_file) == true)) // passes the original file and the signed file
-        std::cout << "The document is authentic\n";
-      else
-        std::cout << "The document is modified\n";
 
-    }
-  }
-  */
   return 0;
 }
